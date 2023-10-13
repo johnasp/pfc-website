533bb004bf955cd82eea5a2e01dcdb0e28991c61
John Aspinall <john.aspinall73@gmail.com>
2023-10-13 11:41:56 +0100

    Gulp installed

--- README.md
+++
@@ -1,3 +0,0 @@
-# John's 7-1 SASS boilerplate 
-
-A starting file architecture for a web project using SASS.
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+function buildStyles() {
+  return src('sass/main.scss')
+    .pipe(sourcemaps.init()) // Initialize source maps
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS()) // Minify the CSS
+    .pipe(sourcemaps.write('./maps')) // Write source maps to a separate directory
+    .pipe(dest('css'));
+}
+
+function watchTask() {
+  watch(['sass/main.scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
